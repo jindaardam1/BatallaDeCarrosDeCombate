83619bb7565c48308c6aeb35f9456702d7e9342c
Jagoba Inda <jindaardam1@gmail.com>
2023-05-17 03:50:16 +0200

    Empezado a refactorizar el proyecto para utilizar el nuevo sistema de paquetes

---
+++ src/main/java/dao/InicializarBADA.java
@@ -0,0 +1,68 @@
+package dao;
+
+
+import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class InicializarBADA {
+
+    public static void crearBADA() {
+        if (!existeBADA()) {
+            crearBaseDeDatos();
+            System.out.println("Base de datos creada.");
+            return;
+        }
+        System.out.println("Ya existe la base de datos.");
+    }
+
+    public static boolean existeBADA() {
+        File archivoBD = new File("tanquesbd.db");
+        return archivoBD.exists();
+    }
+
+    public static void crearBaseDeDatos() {
+        String url = "jdbc:sqlite:src\\main\\resources\\tanquesbd.db";
+        String sql = obtenerScriptCreacionBADA();
+
+        try (Connection connection = DriverManager.getConnection(url);
+             Statement statement = connection.createStatement()) {
+
+            Class.forName("org.sqlite.JDBC");
+
+            statement.executeUpdate(sql);
+            System.out.println("Base de datos creada exitosamente");
+
+        } catch (ClassNotFoundException e) {
+            System.err.println("Error al cargar el driver JDBC: " + e.getMessage());
+        } catch (SQLException e) {
+            System.err.println("Error al crear la base de datos: " + e.getMessage());
+        }
+    }
+
+
+
+    private static String obtenerScriptCreacionBADA() {
+        StringBuilder script = new StringBuilder();
+
+        try (InputStream inputStream = InicializarBADA.class.getResourceAsStream("/sql/creacionBADA.sql");
+             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+
+            String linea;
+            while ((linea = reader.readLine()) != null) {
+                script.append(linea).append('\n');
+            }
+        } catch (IOException | NullPointerException e) {
+            System.err.println("Error al leer el script de creación de la base de datos: " + e.getMessage());
+        }
+
+        return script.toString();
+    }
+
+
+    public static void main(String[] args) {
+        crearBADA();
+    }
+}
