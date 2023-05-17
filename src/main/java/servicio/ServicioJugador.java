f6160fed58b3201d560b8f0eb06bf95cf8e8cf65
Jagoba Inda <jindaardam1@gmail.com>
2023-05-17 13:58:47 +0200

    Empezados servicios

---
+++ src/main/java/servicio/ServicioJugador.java
@@ -0,0 +1,32 @@
+package servicio;
+
+import dao.ConsultasBADA;
+import dao.InsertsBADA;
+
+import java.util.List;
+
+public class ServicioJugador {
+
+    public static void logingJugador(String jugadorNickname) {
+        if (existeJugador(jugadorNickname)) {
+
+        } else {
+            insertarJugador(jugadorNickname);
+        }
+    }
+
+    private static boolean existeJugador(String jugadorNickname){
+        List<String> jugadores = ConsultasBADA.obtenerJugadores();
+
+        for (String jugador : jugadores) {
+            if (jugador.equals(jugadorNickname)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static void insertarJugador(String jugadorNickname) {
+        InsertsBADA.insertarJugador(jugadorNickname);
+    }
+}
